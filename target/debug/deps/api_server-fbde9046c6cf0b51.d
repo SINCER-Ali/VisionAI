f0be32c412c5a263190c6195524e03a73a00a0ac
nina <thininathinina8@gmail.com>
2026-03-26 00:07:59 +0100

    exposer les  fontions  sur python

--- target/debug/deps/api_server-fbde9046c6cf0b51.d
+++
@@ -1,5 +0,0 @@
-C:\Users\thini\Desktop\PA\VisionAI\target\debug\deps\api_server-fbde9046c6cf0b51.d: api_server\src\main.rs
-
-C:\Users\thini\Desktop\PA\VisionAI\target\debug\deps\libapi_server-fbde9046c6cf0b51.rmeta: api_server\src\main.rs
-
-api_server\src\main.rs:
